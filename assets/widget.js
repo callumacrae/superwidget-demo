0e69b12099d0a2ab5b3a10f1e5cc23f257b44e65
Callum Macrae <callum@macr.ae>
2014-09-26 14:24:19 +0100

    Retain scroll position

--- assets/widget.js
+++ assets/widget.js
@@ -1,15 +1,34 @@
 'use strict';
 
+var RATIO = 990 / 120;
+
+var $widget = $('.widget');
+var portrait;
+var scrollCurrent;
+
+$widget.scroll(function () {
+	if (portrait) {
+		scrollCurrent = $widget.scrollTop();
+	} else {
+		scrollCurrent = $widget.scrollLeft() / RATIO;
+	}
+});
+
 $(window).on('orientationchange resize', function () {
-	var portrait = (window.innerHeight < window.innerWidth);
+	portrait = (window.innerHeight > window.innerWidth);
 
 	$('body').removeClass('landscape portrait')
-		.addClass(portrait ? 'landscape' : 'portrait');
+		.addClass(portrait ? 'portrait' : 'landscape');
+
+	if (portrait) {
+		$widget.scrollTop(scrollCurrent);
+	} else {
+		$widget.scrollLeft(scrollCurrent * RATIO);
+	}
 });
 
 $(window).triggerHandler('orientationchange');
 
-var $widget = $('.widget');
 $('.js-widget-open').on('click', function () {
 	$widget.fadeIn();
 });
