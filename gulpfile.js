f18016c752955b0659fae123f2a3c1a993034540
Callum Macrae <callum@macr.ae>
2014-10-01 15:50:15 +0100

    Added heidelburg

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,15 @@ gulp.task('sass', function () {
 		.pipe(gulp.dest('build'));
 });
 
+gulp.task('sass-heidelberg', function () {
+	return gulp.src(['heidelberg/sass/styles.sass'])
+		.pipe(plugins.rubySass())
+		.pipe(plugins.plumber())
+		.pipe(plugins.autoprefixer())
+//		.pipe(plugins.minifyCss())
+		.pipe(gulp.dest('heidelberg/css'));
+});
+
 gulp.task('browser-sync', function () {
 	browserSync.init([
 		'build/*.css',
@@ -33,6 +42,27 @@ gulp.task('browser-sync', function () {
 	});
 });
 
+gulp.task('browser-sync-heidelberg', function () {
+	browserSync.init([
+		'heidelberg/css/*.css',
+		'heidelberg/js/**/*.js',
+		'heidelberg/**/*.html'
+	], {
+		server: {
+			baseDir: 'heidelberg'
+		},
+		ghostMode: {
+			scroll: true,
+			links: true,
+			forms: true
+		}
+	});
+});
+
 gulp.task('default', ['sass', 'browser-sync'], function () {
 	gulp.watch('assets/**/*.scss', ['sass']);
+});
+
+gulp.task('heidelberg', ['sass-heidelberg', 'browser-sync-heidelberg'], function () {
+	gulp.watch('heidelberg/**/*.scss', ['sass-heidelberg']);
 });
